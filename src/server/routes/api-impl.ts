6268b06aca625bd6f437325a7f8099eabb3bfc75
Rajesh Kamisetty <rajesh.kamisetty@gmail.com>
2017-05-20 21:18:19 -0500

    handle svg and json media types

--- src/server/routes/api-impl.ts
+++ src/server/routes/api-impl.ts
@@ -1,5 +1,6 @@
-var vizJs = require('viz.js');
-var loadedData = require("../LoadedData");
+const vizJs = require('viz.js');
+const loadedData = require("../LoadedData");
+const util = require('util');
 import { BudNode } from "../data/BudNode";
 import { BudNodeMatcher } from "../BudNodeMatcher";
 
@@ -32,6 +33,12 @@ module.exports = {
         var maxLevel = req.query.level == 0 ? 100 : req.query.level;
         matcher.buildGraph(req.params.name, (req.query.view == "detailed"), maxLevel, null, req.query.showCommonOnly);
         //console.log(" Cluster sizes", matcher.getGraph().toString());
-        res.send(vizJs(matcher.getGraph().toString()));
+        if(req.accepts('application/svg+xml')){
+            res.send(vizJs(matcher.getGraph().toString()));
+        }   
+        if(req.accepts('application/json')){
+            //console.log(util.inspect(matcher.getGraph(), { showHidden: true, depth: null }));
+            res.send(JSON.stringify(matcher.getGraph().toString()));
+        }        
     }
 }
