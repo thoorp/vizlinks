452b096973c3db98e4b1d0633015b0cdbe7cf9e0
Rajesh Kamisetty <rajesh.kamisetty@gmail.com>
2017-05-21 21:03:11 -0500

    add missing config data

--- src/server/LoadedData.ts
+++ src/server/LoadedData.ts
@@ -1,8 +1,9 @@
 import { BudNodes } from "./data/BudNodes";
 import { Links } from "./data/Links";
+var config = require("./configfiles/NodeTypeConfig.json");
 
 module.exports = {
     budNodesInstance: new BudNodes(),
     linksInstance: new Links(),
-    configData:[]
+    configData: config
 }
